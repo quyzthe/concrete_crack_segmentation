79e73e49f99c8c42f1d7afa0d34ef489bca05edd
The Tran <132347381+quyzthe@users.noreply.github.com>
2025-06-14 20:06:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -119,30 +119,30 @@ if uploaded_file is not None:
         st.subheader("🖼️ So sánh ảnh trước và sau phân tích")
         col1, col2 = st.columns([1,1], gap="large")
         with col1:
-            st.image(image, use_container_width=True)
-            st.markdown("<div class='image-caption'>Ảnh gốc</div>", unsafe_allow_html=True)
-        with col2:
             st.image(result_image, use_container_width=True)
             st.markdown("<div class='image-caption'>Kết quả phân vùng</div>", unsafe_allow_html=True)
+        with col2:
+            st.image(image, use_container_width=True)
+            st.markdown("<div class='image-caption'>Ảnh gốc</div>", unsafe_allow_html=True)
 
         os.unlink(file_path)
 
     elif uploaded_file.type == 'video/mp4':
-        col1, col2 = st.columns([1, 1])
-        with col1:
-            st.video(file_path)
-            st.markdown("<div class='image-caption'>🎥 Video gốc</div>", unsafe_allow_html=True)
+        st.subheader("🎥 Video gốc")
+        st.video(file_path)
 
-        output_path = None
         if st.button("▶️ Bắt đầu xử lý video"):
             with st.spinner("⚙️ Đang xử lý video..."):
                 output_path = process_video_streamlit(file_path, model, transform_img, device)
             st.success("✅ Video đã được xử lý xong!")
 
-        if output_path:
-            with col2:
+            col1, col2 = st.columns([1, 1])
+            with col1:
                 st.video(output_path)
-                st.markdown("<div class='image-caption'>🎯 Video kết quả</div>", unsafe_allow_html=True)
+                st.markdown("<div class='image-caption'>Video kết quả</div>", unsafe_allow_html=True)
+            with col2:
+                st.video(file_path)
+                st.markdown("<div class='image-caption'>Video gốc</div>", unsafe_allow_html=True)
 
 # ==== FOOTER ==== #
 st.markdown("<div class='footer'>Developed by <b>Trần Quý Thế – 20THXD1</b></div>", unsafe_allow_html=True)
